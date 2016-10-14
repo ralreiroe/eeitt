5c58a33798d60a6220d5ec414e4a87af14729f8c
milosz-muszynski <milosz.muszynski@digital.hmrc.gov.uk>
2016-10-14 12:45:46 +0100

    integrated Peter's changes

--- app/uk/gov/hmrc/eeitt/controllers/EnrolmentController.scala
+++ app/uk/gov/hmrc/eeitt/controllers/EnrolmentController.scala
@@ -4,6 +4,8 @@ import play.api.libs.json.Json
 import uk.gov.hmrc.play.microservice.controller.BaseController
 import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
 import play.api.mvc._
+import reactivemongo.bson.BSONObjectID
+import uk.gov.hmrc.eeitt.model.Enrolment
 import uk.gov.hmrc.eeitt.services.EnrolmentStoreService
 
 import scala.concurrent.Future
@@ -18,6 +20,36 @@ trait EnrolmentController extends BaseController {
     enrolmentStoreService.getEnrolments().map {
       enrolmentList => Ok(Json.toJson(enrolmentList))
     }
+  }
+
+  def check = Action.async { implicit request =>
+
+    // extract parameters from request
+    val formId = request.getQueryString("formId").getOrElse("")
+    val registrationNo = request.getQueryString("registrationNo").getOrElse("")
+    val postCode = request.getQueryString("postCode").getOrElse("")
+
+    // create enrolled
+    val enrolled = new Enrolment(BSONObjectID.generate, formId, registrationNo, postCode)
+
+    // determine enrolled status
+    val isEnrolled = enrolled.is
+    val isRegistrationNumberOk = enrolled.isRegistrationNumberOk
+    val isValidPostcode = enrolled.isValidPostCode
+
+    // debug
+    println("IS_ENROLLED = " + isEnrolled)
+    println("REGISTRATION_OK = " + isRegistrationNumberOk)
+    println("VALID_POSTCODE = " + isValidPostcode)
+
+    // create json response and return it
+    //val json: JsValue = JsObject(Seq("isEnrolled" ->JsBoolean(isEnrolled),"isRegistrationNumberOk" ->JsBoolean(isRegistrationNumberOk),"isValidPostcode" ->JsBoolean(isValidPostcode)))
+    //Future.successful(Ok(json))
+
+    // return result to page
+    //    Future.successful(Ok(enrolment.render(isEnrolled.toString, isRegistrationNumberOk.toString, isValidPostcode.toString, Enrolled.table)))
+    Future.successful(Ok)
+
   }
 
 }
--- app/uk/gov/hmrc/eeitt/model/Enrolment.scala
+++ app/uk/gov/hmrc/eeitt/model/Enrolment.scala
@@ -4,9 +4,21 @@ import play.api.libs.json.{ Format, Json, OFormat }
 import reactivemongo.bson.BSONObjectID
 import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
 
-case class Enrolment(id: BSONObjectID, formId: String, registrationId: String, postalCode: String)
+import scala.util.matching.Regex
+
+case class Enrolment(id: BSONObjectID, formId: String, registrationId: String, postalCode: String) {
+  def is = Enrolment.table.contains(this)
+
+  def isRegistrationNumberOk = registrationId.contains(formId)
+
+  val postCodeRegex = """^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$"""
+
+  def isValidPostCode = new Regex(postCodeRegex).findFirstIn(this.postalCode).isDefined
+
+}
 
 object Enrolment {
+  val table = List(Enrolment(BSONObjectID.generate, "1", "1", "PO197XF"), Enrolment(BSONObjectID.generate, "2", "2", "2"), Enrolment(BSONObjectID.generate, "3", "3", "3"))
   private implicit val BSONObjectIDFormat = ReactiveMongoFormats.objectIdFormats
   implicit val mongoFormats: Format[Enrolment] = Json.format[Enrolment]
   implicit val oFormat: OFormat[Enrolment] = Json.format[Enrolment]
