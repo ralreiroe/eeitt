20f902d7a78a80d30969e2f94b4dc9ce55c13183
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2016-10-24 11:12:51 +0100

    localization of response messages

--- app/uk/gov/hmrc/eeitt/model/EnrolmentVerificationResponse.scala
+++ app/uk/gov/hmrc/eeitt/model/EnrolmentVerificationResponse.scala
@@ -1,5 +1,6 @@
 package uk.gov.hmrc.eeitt.model
 
+import play.api.i18n.Messages
 import play.api.libs.json.{ Format, Json }
 
 case class EnrolmentVerificationResponse(error: Option[String])
@@ -7,11 +8,11 @@ case class EnrolmentVerificationResponse(error: Option[String])
 object EnrolmentVerificationResponse {
   implicit val enrolmentResponseFormat: Format[EnrolmentVerificationResponse] = Json.format[EnrolmentVerificationResponse]
   val RESPONSE_OK = this(None)
-  val RESPONSE_NOT_FOUND = this(Some("not found"))
-  val MULTIPLE_FOUND = this(Some("multiple found"))
-  val INCORRECT_REGIME = this(Some("incorrect regime"))
-  val INCORRECT_POSTCODE = this(Some("incorrect postcode"))
-  val INCORRECT_ARN = this(Some("incorrect ARN"))
-  val MISSING_ARN = this(Some("missing ARN"))
-  val INCORRECT_ARN_FOR_CLIENT = this(Some("incorrect ARN for client"))
+  val RESPONSE_NOT_FOUND = this(Some(Messages("verification.response.not.found.msg")))
+  val MULTIPLE_FOUND = this(Some(Messages("verification.response.multiple.found.msg")))
+  val INCORRECT_REGIME = this(Some(Messages("verification.response.incorrect.regime.msg")))
+  val INCORRECT_POSTCODE = this(Some(Messages("verification.response.incorrect.postcode.msg")))
+  val INCORRECT_ARN = this(Some(Messages("verification.response.incorrect.arn.msg")))
+  val MISSING_ARN = this(Some(Messages("verification.response.missing.arn.msg")))
+  val INCORRECT_ARN_FOR_CLIENT = this(Some(Messages("verification.response.incorrect.arn.for.client.msg")))
 }
