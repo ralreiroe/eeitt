e77c8450a35e90f3ad40d82dc91c152ed8b42fa3
Konrad Wudkowski <wudkowski@gmail.com>
2016-11-15 08:27:41 +0000

    added error handling for non-parsable lines

--- app/uk/gov/hmrc/eeitt/controllers/EtmpDataLoaderController.scala
+++ app/uk/gov/hmrc/eeitt/controllers/EtmpDataLoaderController.scala
@@ -5,10 +5,12 @@ import play.api.mvc.Action
 import reactivemongo.api.commands.MultiBulkWriteResult
 import uk.gov.hmrc.eeitt.repositories._
 import uk.gov.hmrc.eeitt.services.{ EtmpDataParser, LineParsingException }
+import uk.gov.hmrc.eeitt.utils.NonFatalWithLogging
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{ Failure, Success, Try }
 
 trait EtmpDataLoaderController extends BaseController {
 
@@ -22,21 +24,27 @@ trait EtmpDataLoaderController extends BaseController {
 
   def load[A](parseFile: String => Seq[A], replaceAll: Seq[A] => Future[MultiBulkWriteResult]) =
     Action.async(parse.tolerantText) { implicit request =>
-      val records = parseFile(request.body)
-      val expectedNumberOfInserts = records.size
-      replaceAll(records).map { writeResult =>
-        if (writeResult.ok && writeResult.n == expectedNumberOfInserts) {
-          Created(Json.obj("message" -> s"$expectedNumberOfInserts unique objects imported successfully"))
-        } else {
-          InternalServerError(
-            Json.obj(
-              "message" -> s"Failed to replace existing records with $expectedNumberOfInserts new ones",
-              "details" -> writeResult.toString
-            )
-          )
-        }
-      }.recover {
-        case LineParsingException(msg) => InternalServerError(Json.obj("message" -> msg))
+      Try(parseFile(request.body)) match {
+        case Success(records) =>
+          replaceAll(records).map { writeResult =>
+            val expectedNumberOfInserts = records.size
+            if (writeResult.ok && writeResult.n == expectedNumberOfInserts) {
+              Created(Json.obj("message" -> s"$expectedNumberOfInserts unique objects imported successfully"))
+            } else {
+              InternalServerError(
+                Json.obj(
+                  "message" -> s"Failed to replace existing records with $expectedNumberOfInserts new ones",
+                  "details" -> writeResult.toString
+                )
+              )
+            }
+          }
+
+        case Failure(LineParsingException(msg)) =>
+          Future.successful(BadRequest(Json.obj("message" -> msg)))
+
+        case Failure(NonFatalWithLogging(e)) =>
+          Future.successful(InternalServerError(Json.obj("message" -> e.getMessage)))
       }
     }
 
