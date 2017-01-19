11080ac3fce69f11ec9a61f8e0f269818ea3060c
Dave English <dave.english@digital.hmrc.gov.uk>
2017-01-19 14:39:35 +0000

    Initial add of proposed prepop endpoints

---
+++ app/uk/gov/hmrc/eeitt/controllers/PrepopDataController.scala
@@ -0,0 +1,27 @@
+package uk.gov.hmrc.eeitt.controllers
+
+import play.api.mvc._
+import uk.gov.hmrc.play.microservice.controller.BaseController
+
+import scala.concurrent.Future
+
+trait PrepopDataControllerHelper extends BaseController {
+  def get(dataType: String, id: String) = Action.async {
+    val d: Option[String] = ???
+    d match {
+      case Some(jsonData) => Future.successful(Ok(jsonData))
+      case None => Future.successful(NotFound)
+    }
+  }
+
+  def put(dataType: String, id: String, jsonData: String) = Action.async {
+    Future.successful(Ok)
+  }
+
+  def delete(dataType: String, id: String) = Action.async {
+    Future.successful(Ok)
+  }
+
+}
+
+class PrepopDataController extends PrepopDataControllerHelper {}
