4b0c1653dd5e8dd627c29994463911d790ef66ed
Dave English <dave.english@digital.hmrc.gov.uk>
2017-02-01 11:09:49 +0000

    Initial save4later dependency, compiles tests and runs

--- app/uk/gov/hmrc/eeitt/controllers/PrepopDataController.scala
+++ app/uk/gov/hmrc/eeitt/controllers/PrepopDataController.scala
@@ -1,12 +1,18 @@
 package uk.gov.hmrc.eeitt.controllers
 
 import play.api.mvc._
+import uk.gov.hmrc.eeitt.MicroserviceShortLivedCache
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 import scala.concurrent.Future
 
 trait PrepopDataControllerHelper extends BaseController {
   def get(dataType: String, id: String) = Action.async {
+    //    val x = MicroserviceShortLivedCache.fetchAndGetEntry(id, dataType)
+    //    x match {
+    //      case Some(s) => ???
+    //      case _ => ???
+    //    }
     val d: Option[String] = ???
     d match {
       case Some(jsonData) => Future.successful(Ok(jsonData))
--- app/uk/gov/hmrc/eeitt/microserviceWiring.scala
+++ app/uk/gov/hmrc/eeitt/microserviceWiring.scala
@@ -1,5 +1,7 @@
 package uk.gov.hmrc.eeitt
 
+import uk.gov.hmrc.crypto.ApplicationCrypto
+import uk.gov.hmrc.http.cache.client.{ SessionCache, ShortLivedCache, ShortLivedHttpCaching }
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
@@ -22,3 +24,19 @@ class MicroserviceAuditConnector(val wsApi: WSAPI) extends AuditConnector with R
 object MicroserviceAuthConnector extends AuthConnector with ServicesConfig {
   override val authBaseUrl = baseUrl("auth")
 }
+
+object MicroserviceShortLivedHttpCaching extends ShortLivedHttpCaching with AppName with ServicesConfig {
+  override lazy val http = WSHttp
+  override lazy val defaultSource = appName
+  override lazy val baseUri = baseUrl("cachable.short-lived-cache")
+  override lazy val domain = getConfString(
+    "cachable.short-lived-cache.domain",
+    throw new Exception(s"Could not find config 'cachable.short-lived-cache.domain'")
+  )
+}
+
+object MicroserviceShortLivedCache extends ShortLivedCache {
+  override implicit lazy val crypto = ApplicationCrypto.JsonCrypto
+  override lazy val shortLiveCache = MicroserviceShortLivedHttpCaching
+}
+
